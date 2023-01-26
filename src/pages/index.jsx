f16c7fcf4c9a1c269496f6a7488ba18b6a27b26a
John Honeyman <johnhoneyman10@gmail.com>
2023-01-26 15:43:53 +0800

    Finished Gatsby Tutorial Part 3

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by followign the Gatsby Tutorials.</p>
+      <StaticImage alt="The Gatsby Logo Stored in Local" src="../images/icon.png" />
     </Layout>
   );
 };
