9eb962afefae39ce16afe0ec02ffa97c29fc9da3
John Honeyman <johnhoneyman10@gmail.com>
2023-01-26 08:44:32 +0800

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main>
+      <div>Home</div>
+      <div>This is another page</div>
+    </main>
+  );
+};
+
+export const Head = () => <title>Home Page</title>;
+
+export default Home;
